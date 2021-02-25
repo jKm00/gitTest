abb4be97dbb5f44c435ce3d7b9f5e7511179f181
Joakim <joakied@stud.ntnu.no>
2021-02-25 13:05:56 +0100

    Started on phonebook class

---
+++ src/main/java/no/ntnu/idata2001/PhoneBook.java
@@ -0,0 +1,13 @@
+package no.ntnu.idata2001;
+
+import java.util.ArrayList;
+
+public class PhoneBook
+{
+    private ArrayList<Person> persons;
+
+    public PhoneBook()
+    {
+        this.persons = new ArrayList<>();
+    }
+}
